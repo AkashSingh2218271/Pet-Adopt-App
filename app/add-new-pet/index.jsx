e52d14636e57a25d3ac2cd94bfd636d82b766cef
AkashSingh2218271 <akrawat0056@gmail.com>
2025-04-26 15:59:46 +0530

    Adding new function

--- app/add-new-pet/index.jsx
+++ app/add-new-pet/index.jsx
@@ -1,10 +1,38 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, TextInput } from 'react-native'
+import React, { useEffect } from 'react'
+import { useNavigation } from 'expo-router'
+import { Colors } from './../../constants/Colors'
 
 export default function AddNewPet() {
+    const navigation=useNavigation();
+
+    useEffect(()=>{
+        navigation.setOptions({
+            headerTitle:'Add New Pet'
+        })
+    }, [])
+
   return (
-    <View>
-      <Text>AddNewPet</Text>
+    <View style={{
+        padding:20
+    }}>
+      <Text style={{
+        fontFamily:'outfit-medium',
+        fontSize:20
+      }}>Add New Pet for adoption</Text>
+      <Image source={require('../../assets/images/placeholder.png')}
+        style={{
+            width:100,
+            height:100,
+            borderRadius:15,
+            borderWidth:1,
+            borderColor:Colors.GRAY
+        }}
+      />
+      <View>
+        <Text>Pet Name*</Text>
+        <TextInput placeholder=''/>
+      </View>
     </View>
   )
 }
